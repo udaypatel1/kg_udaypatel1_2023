c3c231d49d3148f4e03b0f0f524e117f06527f59
Uday Patel <uday3patel@gmail.com>
2021-05-01 21:29:06 -0400

    Number to phonetic equivalent hashmap created

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let numberMap = new Map();
+
+numberMap.set("1", "One");
+numberMap.set("2", "Two");
+numberMap.set("3", "Three");
+numberMap.set("4", "Four");
+numberMap.set("5", "Five");
+numberMap.set("6", "Six");
+numberMap.set("7", "Seven");
+numberMap.set("8", "Eight");
+numberMap.set("9", "Nine");
+
